7ce62378edee0e6caecc382fdabed2264beb536d
senjakortesaari <senjakortesaari@hotmail.com>
2022-11-10 16:33:22 +0100

    moveable implemented

--- Car.java
+++ Car.java
@@ -1,13 +1,58 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-abstract class Car {
+abstract class Car implements Movable {
 
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
+    public double x;
+    public double y;
+
+    double[][] directionList = {
+        {0, 1},
+        {1, 0},
+        {0, -1},
+        {-1, 0}
+    };
+
+    //speed x index 0
+    //speed y index 1
+    // går man till höger -> nästa index i listan
+    // går man till vänster -> föregående index i listan
     
+
+    int directionIndex = 0;
+
+    public void move() {
+        x += currentSpeed * directionList[directionIndex][0];
+        y += currentSpeed * directionList[directionIndex][1];
+    }
+
+    public void turnLeft(){
+        directionIndex -= 1;
+        if (directionIndex == -1){
+            directionIndex = 3;
+        }
+    }
+
+    public void turnRight(){
+        directionIndex += 1;
+        if (directionIndex == 4){
+            directionIndex = 0;
+        }
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
     public int getNrDoors(){
         return nrDoors;
     }
