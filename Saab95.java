599eb31ea54d7ca269cbdd8d6ccb3a9e8fb681b2
senjakortesaari <senjakortesaari@hotmail.com>
2022-11-10 14:06:00 +0100

    Inheritance

--- Saab95.java
+++ Saab95.java
@@ -1,13 +1,7 @@
 import java.awt.*;
 
-public class Saab95{
-
+class Saab95 extends Car {
     public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
     
     public Saab95(){
         nrDoors = 2;
@@ -18,32 +12,7 @@ public class Saab95{
         stopEngine();
     }
     
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
-
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
+    
 
     public void setTurboOn(){
 	    turboOn = true;
@@ -53,19 +22,22 @@ public class Saab95{
 	    turboOn = false;
     }
     
+    @Override
     public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
-
+    @Override
     public void incrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() + speedFactor() * amount;
     }
-
+    @Override
     public void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
+    
+
     
     // TODO fix this method according to lab pm
     public void gas(double amount){
