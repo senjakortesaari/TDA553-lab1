a2d2ac497bb1323b391d6ac559c6c7b888547738
Idaaberg <idaaberg0310@gmail.com>
2022-11-11 10:34:11 +0100

    jUnit

---
+++ jUnitTestFile.java
@@ -0,0 +1,16 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MyFirstJUnitJupiterTests {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void addition() {
+        assertEquals(2, calculator.add(1, 1));
+    }
+
+}
+
